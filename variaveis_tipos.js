768e3efcd7d5efbb141b1414686ec18abb93670f
RODRIGO BARBOSA PEREIRA <rodrigo.bpereira@a.ucb.br>
2025-02-27 10:48:52 -0300

    Conteúdos apredidos na aula 1

---
+++ variaveis_tipos.js
@@ -0,0 +1,25 @@
+console.log("Ola mundo");
+
+const a = 3;
+console.log("a = "+a);
+
+var b = 3;
+var b = 5;
+console.log("b = "+b);
+
+let c = 7;
+c = 9;
+console.log("c = "+c);
+
+let nome = "Rodrigo";
+let telefone = "1234"
+console.log(`Meu nome é ${nome} e meu telefoe é ${telefone}`);
+
+let d = 1.2;
+d = 3.14;
+console.log("d = "+d);
+
+let verdadeiro = true;
+let falso = false;
+
+let conexao = undefined;
