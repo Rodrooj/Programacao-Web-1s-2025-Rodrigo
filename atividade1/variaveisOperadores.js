5b16533a3d9b75dd76fba00c7a65988cf4b08300
RODRIGO BARBOSA PEREIRA <rodrigo.bpereira@a.ucb.br>
2025-02-27 12:25:28 -0300

    Conclusão da primeira atividade da matéria

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let num1 = 7
+let num2 = 2;
+
+let rsoma = num1 + num2;
+let rsub = num1 - num2;
+let rmult = num1 * num2;
+let rdiv = num1 / num2;
+
+console.log(`O resultado da soma de ${num1} e ${num2} é ${rsoma}`);
+console.log(`O resultado da subtração de ${num1} e ${num2} é ${rsub}`);
+console.log(`O resultado da divisão de ${num1} e ${num2} é ${rmult}`);
+console.log(`O resultado da divisão de ${num1} e ${num2} é ${rdiv}`);
