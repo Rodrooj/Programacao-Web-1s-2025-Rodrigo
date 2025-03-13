dd0a3b3266ca68c91191e423359fea1ae301da1f
Rodrooj <rodrigobarbosapereira17@gmail.com>
2025-03-12 23:48:14 -0300

    Implementação dos programas propostos na aula 2

--- Atividade2/matrizTransposta.js
+++ Atividade2/matrizTransposta.js
@@ -1,1 +1,17 @@
-transporMatriz(A);
+function transporMatriz(A) {
+    console.log("Matriz original:");
+    A.forEach(linha => console.log(linha.join(" ")));
+
+    const transposta = A[0].map((_, i) => A.map(linha => linha[i]));
+
+    console.log("\nMatriz transposta:");
+    transposta.forEach(linha => console.log(linha.join(" ")));
+}
+
+const matrizExemplo = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+
+transporMatriz(matrizExemplo);
