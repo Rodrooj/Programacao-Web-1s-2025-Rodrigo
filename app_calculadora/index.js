702077fecff8c60b06068404de031f00f7e4437e
RODRIGO BARBOSA PEREIRA <rodrigo.bpereira@a.ucb.br>
2025-03-13 10:33:32 -0300

    Criação da primeira aplicação web, usando express.js

---
+++ app_calculadora/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const calc = require('./calculadora');
+const app = express();
+
+app.get('/ola', (req, res)=>{
+    res.send('Ola mundo');
+})
+
+app.get('/ola/:nome', (req, res)=>{
+    res.send('Ola, ' + req.params.nome);
+})
+
+const PORT = 8080;
+app.listen(PORT, ()=>{
+    console.log('app rodando na porta '+PORT);
+});
